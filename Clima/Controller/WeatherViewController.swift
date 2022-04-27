f1c9966075144d7be9fd0fecaa35ecd906718e99
kuu7life <kuu7life@gmail.com>
2022-04-27 20:59:19 +0800

    view -> background image settings fixed

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherViewController: UIViewController {
     
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
